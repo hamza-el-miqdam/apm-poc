a1985ec7d0c17b9d3d351e379f67b22b2beff91a
hamza-el-miqdam <hamza.elmiqdam@zenika.com>
2024-08-01 11:21:13 +0200

    feat(ELK + Fleet): small refacto

--- startup.sh
+++ startup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+helm repo add elastic https://helm.elastic.co
+helm repo update
+
 kubectl apply -f namespace-apm.yaml
-helm install elasticsearch --values elastic-values.yaml elastic/elasticsearch -n apm
+helm install elasticsearch --values ./elk/elastic-values.yaml elastic/elasticsearch -n apm
 sleep 50
-helm install kibana --values kibana-values.yaml elastic/kibana -n apm
+helm install kibana --values ./elk/kibana-values.yaml elastic/kibana -n apm
+sleep 30
 
-kubectl apply -f fleet-server-rbac.yaml -n apm
-kubectl apply -f fleet-server.yaml -n apm
+kubectl apply -f fleet-server -n apm
 
-# ./microservice-kubernetes/microservice-kubernetes-demo/kubernetes-deploy.sh
+# ./clone-microservice-kubernetes/microservice-kubernetes-demo/kubernetes-deploy.sh
